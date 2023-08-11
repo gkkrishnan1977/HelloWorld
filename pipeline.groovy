e6b546507a702dd80ed675b79adeae430b67893b
gkkrishnan1977 <gkkrishnan1977@gmail.com>
2023-08-11 12:43:17 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,12 +1,14 @@
 def runPipeline() {
-    stage('Checkout') {
-        checkout scm
-    }
-    stage('Compile') {
-        sh 'javac HelloWorld.java'
-    }
-    stage('Test') {
-        sh 'java HelloWorld'
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Compile') {
+            sh 'javac HelloWorld.java'
+        }
+        stage('Test') {
+            sh 'java HelloWorld'
+        }
     }
 }
 
